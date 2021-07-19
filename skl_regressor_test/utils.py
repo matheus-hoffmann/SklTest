000053694e3aedfc3951ba5eff9cbac621bf549d
Matheus Hoffmann <36958635+matheus-hoffmann@users.noreply.github.com>
2021-07-19 13:40:03 -0300

    Add typing and comments

--- skl_regressor_test/utils.py
+++ skl_regressor_test/utils.py
@@ -3,6 +3,23 @@ import pandas as pd
 
 
 def doe_normalize(matrix: np) -> tuple[np, list, list]:
+    """
+    Normalize each column of the matrix between [-1, 1] and return the keys to recompose the original data.
+
+    Parameters
+    ----------
+    matrix : np
+        Original data matrix
+
+    Return
+    ------
+    normalized_matrix : np
+        Normalized matrix
+    x0_list : lst
+        Value of the real variable at the center of the experimental region
+    dx_list : lst
+        Value of the deviation from x0
+    """
     x0_list = []
     dx_list = []
     normalized_matrix = np.zeros(matrix.shape)
@@ -18,6 +35,24 @@ def doe_normalize(matrix: np) -> tuple[np, list, list]:
 
 
 def read_data(filepath: str) -> tuple[pd, np, np]:
+    """
+    Read the data file and return the dataframe and the input/output arrays.
+    It is assumed that is single output (SISO or MISO) analysis, and the last column must be the output.
+
+    Parameters
+    ----------
+    filepath : str
+        Path to the data file
+
+    Return
+    ------
+    df : np
+        Original data in a dataframe
+    input_data : np
+        Normalized input matrix [n x m]
+    output_data : np
+        Output array [n x 1]
+    """
     df = pd.read_csv(filepath)
     original_data = df.to_numpy()
     original_input_data = original_data[:, :-1]
@@ -27,6 +62,20 @@ def read_data(filepath: str) -> tuple[pd, np, np]:
 
 
 def check_max_n_iter(desired_n_iter:int, space:dict) -> int:
+    """
+    Check if the desired number of iterations is higher than the number of hyperparameters combinations.
+
+    Parameters
+    ----------
+    desired_n_iter : int
+        Desired number of iterations in a RandomizedSearchCV
+    space : dict
+        Possible values of each hyperparameter of a Scikit-Learn method
+
+    Return
+    ------
+    Number of iterations to run in RandomizedSearchCV
+    """
     n_comb_keys = 1
     for value in space.values():
         n_comb_keys *= len(value)
@@ -37,6 +86,19 @@ def check_max_n_iter(desired_n_iter:int, space:dict) -> int:
 
 
 def split_dict(dictionary:dict) -> str:
+    """
+    Convert a dict to a string, separating each key by a /
+
+    Parameters
+    ----------
+    dictionary : dict
+        Dictionary to be converted
+
+    Return
+    ------
+    final_text : str
+        String with the dictionary converted
+    """
     keys = list(dictionary.keys())
     values = list(dictionary.values())
     final_text = ""
@@ -49,3 +111,4 @@ def split_dict(dictionary:dict) -> str:
     final_text += ": "
     final_text += str(values[-1])
     return final_text
+
