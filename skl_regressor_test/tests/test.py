f6ec16af6e951651ee3041ce2d6e7e72d445ec24
Matheus Hoffmann <36958635+matheus-hoffmann@users.noreply.github.com>
2021-08-06 16:16:58 -0300

    Testing optimization until a max. error lower than specified

--- skl_regressor_test/tests/test.py
+++ skl_regressor_test/tests/test.py
@@ -15,10 +15,11 @@ SklRegressors = SklRegressorTest(m_input=input_data,
 warnings.filterwarnings("ignore")
 
 # Define Scikit-Learn regression models to run and initialization
-SklRegressors.set_desired_models(models="all")
+models = "all"
+SklRegressors.set_desired_models(models=models)
 
 # Analyze the best random state with default hyperparameters
-SklRegressors.test_random_states(n_random_states=100)
+SklRegressors.test_random_states(n_random_states=10)
 
 # Boost hyperparameters through a k-fold cross-validation holdout
 SklRegressors.initialize_parameters()
@@ -33,6 +34,27 @@ SklRegressors.test_all(n_random_states=2,
                        rkf_cv_n_splits=2,
                        rkf_cv_n_repeats=1,
                        n_rand_iter=1)
+
+# Resample train/test until the error get lower than maxerror
+SklRegressors.initialize_parameters()
+SklRegressors.test_random_states_until(maxerror=2.0,
+                                       n_iter=1000)
+
+# Resample train/test and boost model hyperparameters until get lower than maxerror
+SklRegressors.initialize_parameters()
+SklRegressors.test_spaces_until(rkf_cv_n_splits=2,
+                                rkf_cv_n_repeats=1,
+                                n_rand_iter=10,
+                                maxerror=2.0,
+                                n_iter=15)
+
+# Analyze the best performance between  test_random_states_until and test_spaces_until methods at once
+SklRegressors.initialize_parameters()
+SklRegressors.test_all_until(rkf_cv_n_splits=2,
+                             rkf_cv_n_repeats=1,
+                             n_rand_iter=10,
+                             maxerror=2.0,
+                             n_iter=20)
 
 # Save a summary file with the best hyperparameters configuration and statistical data from the models
 summary_df = SklRegressors.write_log(path="",
