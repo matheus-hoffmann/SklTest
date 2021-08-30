b345424d00302811ac41bb5ee2fb059592592177
Matheus Hoffmann <36958635+matheus-hoffmann@users.noreply.github.com>
2021-08-30 09:52:34 -0300

    Add the desired_metric parameter to test implementation

--- skl_regressor_test/tests/test.py
+++ skl_regressor_test/tests/test.py
@@ -19,26 +19,30 @@ models = "all"
 SklRegressors.set_desired_models(models=models)
 
 # Analyze the best random state with default hyperparameters
-SklRegressors.test_random_states(n_random_states=10)
+SklRegressors.test_random_states(n_random_states=10,
+                                 desired_metric="root_mean_squared_error")
 
 # Boost hyperparameters through a k-fold cross-validation holdout
 SklRegressors.initialize_parameters()
 SklRegressors.test_spaces(n_random_states=2,
                           rkf_cv_n_splits=2,
                           rkf_cv_n_repeats=1,
-                          n_rand_iter=1)
+                          n_rand_iter=1,
+                          desired_metric="root_mean_squared_error")
 
 # Analyze the best performance between  test_random_states and test_spaces methods at once
 SklRegressors.initialize_parameters()
 SklRegressors.test_all(n_random_states=2,
                        rkf_cv_n_splits=2,
                        rkf_cv_n_repeats=1,
-                       n_rand_iter=1)
+                       n_rand_iter=1,
+                       desired_metric="root_mean_squared_error")
 
 # Resample train/test until the error get lower than maxerror
 SklRegressors.initialize_parameters()
 SklRegressors.test_random_states_until(maxerror=2.0,
-                                       n_iter=1000)
+                                       n_iter=1000,
+                                       desired_metric="root_mean_squared_error")
 
 # Resample train/test and boost model hyperparameters until get lower than maxerror
 SklRegressors.initialize_parameters()
@@ -46,7 +50,8 @@ SklRegressors.test_spaces_until(rkf_cv_n_splits=2,
                                 rkf_cv_n_repeats=1,
                                 n_rand_iter=10,
                                 maxerror=2.0,
-                                n_iter=15)
+                                n_iter=15,
+                                desired_metric="root_mean_squared_error")
 
 # Analyze the best performance between  test_random_states_until and test_spaces_until methods at once
 SklRegressors.initialize_parameters()
@@ -54,8 +59,12 @@ SklRegressors.test_all_until(rkf_cv_n_splits=2,
                              rkf_cv_n_repeats=1,
                              n_rand_iter=10,
                              maxerror=2.0,
-                             n_iter=20)
+                             n_iter=20,
+                             desired_metric="root_mean_squared_error")
 
 # Save a summary file with the best hyperparameters configuration and statistical data from the models
 summary_df = SklRegressors.write_log(path="",
                                      filename="skl_regressor_test_summary")
+
+# Print all methods and metrics
+SklRegressors.summary()
